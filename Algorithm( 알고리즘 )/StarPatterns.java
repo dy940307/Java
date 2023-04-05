67e16a635b397586aafc5d341121fbc82d38726e
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-05 16:54:49 +0900

    별 찍기

--- Algorithm( 알고리즘 )/StarPatterns.java
+++ Algorithm( 알고리즘 )/StarPatterns.java
@@ -6,7 +6,6 @@ public class StarPatterns {
 		System.out.println("---------------");
 		System.out.println("1. 5x5 사각형");
 		System.out.println("---------------");
-		
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++)
 				System.out.print("*");
@@ -29,7 +28,6 @@ public class StarPatterns {
 		System.out.println("---------------");
 		System.out.println("3. 역슬래시");
 		System.out.println("---------------");
-
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++) {
 				if(i == j)
@@ -96,7 +94,6 @@ public class StarPatterns {
 			for(int j = 5; j > i; j--) {
 				System.out.print("*");
 			}
-			
 			System.out.println();
 		}
 		
