42b8a2aeabd1d3bc4ed7f56756936e9223dc0c69
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-06 23:31:50 +0900

    배열 중복 값 제거

--- Algorithm( 알고리즘 )/ArrayDuplicateRemover.java
+++ Algorithm( 알고리즘 )/ArrayDuplicateRemover.java
@@ -37,7 +37,8 @@ public class ArrayDuplicateRemover {
 		int arr[] = {1, 6, 4, 4, 5, 6, 1, 2, 5};
 		int[] result = removeDuplicates(arr);
 		
-		System.out.println("기존 제거전 값: " + Arrays.toString(arr));
-		System.out.println("중복 제거한 값: " + Arrays.toString(result));
+		System.out.println("중복 제거 전: " + Arrays.toString(arr));	// [1, 6, 4, 4, 5, 6, 1, 2, 5]
+		System.out.println("중복 제거 후: " + Arrays.toString(result));	// [1, 6, 4, 5, 2]
+
 	}
 }
