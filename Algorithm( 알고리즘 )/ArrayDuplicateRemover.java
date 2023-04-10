a42d9d7e41594c71f0b27a92832474512a00a930
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-10 15:51:22 +0900

    배열 요소 중복 제거

--- Algorithm( 알고리즘 )/ArrayDuplicateRemover.java
+++ Algorithm( 알고리즘 )/ArrayDuplicateRemover.java
@@ -1,8 +1,6 @@
 package study1;
 
 import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
 
 /*
     1. 내부 for문에서 배열의 값을 비교하면서 중복 여부를 체크한다.
@@ -13,9 +11,10 @@ import java.util.Scanner;
  */
 
 public class ArrayDuplicateRemover {
+	// int 배열의 중복값을 제거한 배열을 반환하는 메서드
 	static int[] removeDuplicates(int[] a) {
-		int[] result = new int[a.length];
-		int index = 0;
+		int[] tmpArr = new int[a.length];
+		int idx = 0;
 		boolean found;
 	
 		for(int i = 0; i < a.length; i++) {
@@ -26,19 +25,20 @@ public class ArrayDuplicateRemover {
 					break;
 				}
 			}
-			if(!found) // 내부 for문에서 중복값이면 found가 true, 중복값이 아니면 false이다. 
-				result[index++] = a[i];	
+			if(!found)
+				tmpArr[idx++] = a[i];	
 		}
-		// 중복된 값을 뺀 배열 리턴.
-		return Arrays.copyOf(result, index);	
+		// 중복 제거한 배열 리턴
+		return Arrays.copyOf(tmpArr, idx);	
 	}
 	
 	public static void main(String[] args) {
+		// 배열 생성 및 중복 제거
 		int arr[] = {1, 6, 4, 4, 5, 6, 1, 2, 5};
 		int[] result = removeDuplicates(arr);
 		
-		System.out.println("중복 제거 전: " + Arrays.toString(arr));	// [1, 6, 4, 4, 5, 6, 1, 2, 5]
+		// 중복 제거 전과 제거 후의 값 출력
+		System.out.println("중복 제거 전: " + Arrays.toString(arr));		// [1, 6, 4, 4, 5, 6, 1, 2, 5]
 		System.out.println("중복 제거 후: " + Arrays.toString(result));	// [1, 6, 4, 5, 2]
-
 	}
 }
