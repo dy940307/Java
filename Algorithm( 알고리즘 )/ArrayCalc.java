6d88706c8a17d09e4fcdcf8f0bb8f44fe4615a27
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-10 14:48:03 +0900

    배열의 최댓값, 최솟값, 총합, 평균 구하기

--- Algorithm( 알고리즘 )/ArrayCalc.java
+++ Algorithm( 알고리즘 )/ArrayCalc.java
@@ -3,8 +3,8 @@ package study1;
 import java.util.Scanner;
 
 public class ArrayCalc {
-	// 배열의 최댓값 구하는 메서드
-	static int maxOf(int[] a) {
+	// int[] 배열의 최댓값 구하는 메서드
+	static int maxOfInt(int[] a) {
 		int max = a[0];	
 		for(int i = 1; i < a.length; i++) {
 			if(a[i] > max)
@@ -12,8 +12,8 @@ public class ArrayCalc {
 		}
 		return max;
 	}
-	// 배열의 최솟값 구하는 메서드
-	static int minOf(int[] a) {
+	// int[] 배열의 최솟값 구하는 메서드
+	static int minOfInt(int[] a) {
 		int min = a[0];	
 		for(int i = 1; i < a.length; i++) {
 			if(a[i] < min) 
@@ -21,16 +21,16 @@ public class ArrayCalc {
 		}
 		return min;
 	}
-	// 배열의 총합 구하는 메서드
-	static int sumOf(int[] a) {
+	// int[] 배열의 총합 구하는 메서드
+	static int sumOfInt(int[] a) {
 		int sum = 0;
 		for(int i = 0; i < a.length; i++) {
 			sum += a[i];
 		}
 		return sum;
 	}
-	// 배열의 평균 구하는 메서드
-	static double avgOf(int[] a) {
+	// int[] 배열의 평균 구하는 메서드
+	static double avgOfInt(int[] a) {
 		double avg = (double)sumOf(a) / a.length;
 		return avg;
 	}
@@ -42,22 +42,22 @@ public class ArrayCalc {
 		// 배열 길이를 입력 받아서 그 값을 크기로 하여 배열 생성
 		do {
 			System.out.println("배열 길이 입력(1~10): ");
-			len = sc.nextInt();
+			len = Integer.parseInt(sc.nextLine());
 		} while(len < 1 || len > 10);
 		
 		int[] arr = new int[len];
 		
 		// 직접 값을 입력하여 배열 초기화
 		for(int i = 0; i < arr.length; i++) {
-			System.out.print("배열" + (i+1) + "번째 값 입력: ");
-			arr[i] = sc.nextInt();
+			System.out.print(i + "번 인덱스 값 입력: ");
+			arr[i] = Integer.parseInt(sc.nextLine());
 		}
 		
 		// 배열의 최댓값, 최솟값, 총합, 평균 출력
 		System.out.println();
-		System.out.println("배열의 최댓값: " + maxOf(arr));
-		System.out.println("배열의 최솟값: " + minOf(arr));
-		System.out.println("배열의 총합: " + sumOf(arr));
-		System.out.println("배열의 평균: " + avgOf(arr));
+		System.out.println("배열의 최댓값: " + maxOfInt(arr));
+		System.out.println("배열의 최솟값: " + minOfInt(arr));
+		System.out.println("배열의 총합: " + sumOfInt(arr));
+		System.out.println("배열의 평균: " + avgOfInt(arr));
 	}
 }
