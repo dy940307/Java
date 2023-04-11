f64e6dc38d9dbb8d69d8b268ecf6eac50b1cd978
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-12 03:07:49 +0900

    입력한 수를 한 변으로 하는 정사각형을 *로 출력

--- Algorithm( 알고리즘 )/Square.java
+++ Algorithm( 알고리즘 )/Square.java
@@ -5,27 +5,21 @@ import java.util.Scanner;
 
 public class Square {
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
-		int n = 0;
+		Scanner sc = new Scanner(System.in);
 		
-		// 변의 길이 5를 입력하면 5x5 정사각형, 10을 입력하면 10x10 정사각형 출력.
-		System.out.println("정사각형을 출력합니다.");
-		do {
-			System.out.print("변의 길이: ");
-			n = stdIn.nextInt();
-		} while(n <= 0);
-		System.out.println();
+		// 변의 길이 5를 입력하면 5x5 정사각형, 10을 입력하면 10x10 정사각형
+		System.out.print("변의 길이: ");
+		int sideLength = sc.nextInt();
 		
-		// for문 1개 + repeat() 메서드를 활용해서 정사각형 출력
-		for(int i = 0; i < n; i++) {
-			System.out.println("*".repeat(n));
+		// repeat() 메서드를 활용해서 정사각형 출력
+		for(int i = 0; i < sideLength; i++) {
+			System.out.println("*".repeat(sideLength));
 		}
-		
 		System.out.println();
 		
-		// 2중 for문을 사용해 정사각형 출력 
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
+		// 2중 for문을 활용해서 정사각형 출력
+		for(int i = 0; i < sideLength; i++) {
+			for(int j = 0; j < sideLength; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
