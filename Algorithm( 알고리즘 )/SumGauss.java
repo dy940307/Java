160be77d5c895873a0862230d8b01ebdc0cc9208
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-12 03:15:05 +0900

    1부터 n까지의 합 구하기

--- Algorithm( 알고리즘 )/SumGauss.java
+++ Algorithm( 알고리즘 )/SumGauss.java
@@ -17,26 +17,25 @@ import java.util.Scanner;
     	 ㄴ 30 / 2 = 15
      	 ㄴ 1 ~ 5까지의 합은 15.
   
-	2. 가우스의 덧셈 공식 : (n * (n + 1)) / 2
-	 ㄴ 1 ~ 5까지의 합을 구하시오 -> (5 * (5 + 1)) / 2 = 15
+	2. 가우스의 덧셈 공식 : n * (n + 1) / 2
+	 ㄴ 1 ~ 5까지의 합을 구하시오 -> 5 * (5 + 1) / 2 = 15
 		
 */
 
 public class SumGauss {		
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int n;
 		
-		// n값 입력, 양의 정수를 입력할 때까지 반복하는 do-while문.
-		do {
-			System.out.print("n값: ");
-			n = stdIn.nextInt();
+		do { // 사용자로부터 1 이상의 값을 입력받는다
+			System.out.print("n값 입력: ");
+			n = sc.nextInt();
 		} while(n <= 0);
 		
-		// 가우스의 덧셈 공식을 활용하여 1부터 n까지의 합 구하기.
-		int sum = (n * (n + 1)) / 2;
+		// 가우스의 덧셈을 활용하여 1부터 n까지의 합을 구해서 sum 변수에 저장.
+		int sum = n * (n + 1) / 2;
 		
-		// 1부터 n까지의 합을 저장한 sum 변수 출력.
+		// 결괏값 출력.
 		System.out.println("1부터" + n + "까지의 합은" + sum + "입니다.");
 	}
 }
