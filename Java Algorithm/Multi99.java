27fa086046fd92d380c718a0ee31b89571617506
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-14 20:39:49 +0900

    Java Algorithm: 구구단 여러 가지 형태로 출력하기

--- Java Algorithm/Multi99.java
+++ Java Algorithm/Multi99.java
@@ -3,17 +3,6 @@ package study;
 public class Multi99 {
 	public static void main(String[] args) {
 		
-		/* x랑 y값 구하기용 코드
-		for(int i = 0; i < 3; i++)
-			System.out.println(i * 2 + 3);
-		
-		for(int j = 0; j < 5; j++)
-			System.out.println(j % 5 + 1);
-		
-		System.out.println();
-		*/
-		
-		
 		/* 1. 아래와 같은 구구단 출력하기
 			 2 x 1 = 2
 			 2 x 2 = 4
@@ -70,5 +59,38 @@ public class Multi99 {
 			System.out.println();
 		}
 		System.out.println();
-	}
+		
+		
+		
+		/* 3. 아래와 같은 구구단 출력하기
+		
+		2 x 1 = 2	3 x 1 = 3	4 x 1 = 4	
+		2 x 2 = 4	3 x 2 = 6	4 x 2 = 8	
+		2 x 3 = 6	3 x 3 = 9	4 x 3 = 12	
+		
+		5 x 1 = 5	6 x 1 = 6	7 x 1 = 7	
+		5 x 2 = 10	6 x 2 = 12	7 x 2 = 14	
+		5 x 3 = 15	6 x 3 = 18	7 x 3 = 21	
+
+		8 x 1 = 8	9 x 1 = 9	
+		8 x 2 = 16	9 x 2 = 18	
+		8 x 3 = 24	9 x 3 = 27
+		
+		*/
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 3 ; j++) {
+				int x = (j + 2) + i / 3 * 3;
+				int y = i % 3 + 1;
+				
+				if(x > 9) // 9단까지만 출력
+					break;
+				
+				System.out.printf("%d x %d = %d\t", x, y, x * y);
+			}
+			System.out.println();
+			if(i % 3 == 2) // 4단, 7단 등의 줄바꿈에 활용하기 위한 if문
+				System.out.println();
+		}
+	
+	} // main의 끝
 }
