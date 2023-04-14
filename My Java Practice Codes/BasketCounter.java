80ac2f8f7cda8c00557c5b3878f94e3e1ce307a2
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-14 21:29:52 +0900

    Java Code: 사과를 담기 위해 필요한 바구니 개수 구하기

--- My Java Practice Codes/BasketCounter.java
+++ My Java Practice Codes/BasketCounter.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class BasketCounter {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int appleCount;					// 입력한 사과 개수를 저장할 변수
+		int appleCount;				// 입력한 사과 개수를 저장할 변수
 		final int BASKET_SIZE = 10;		// 바구니 1개에 담을 수 있는 사과의 개수
 	
 		do {
