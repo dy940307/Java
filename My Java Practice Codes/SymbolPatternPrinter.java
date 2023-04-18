a7d6676495bf0945ef1bac595c5dd932e0c8c33f
dy940307 <129681203+dy940307@users.noreply.github.com>
2023-04-18 10:10:44 +0900

    Java Code: +와 - 번갈아 가며 n개만큼 출력

--- My Java Practice Codes/SymbolPatternPrinter.java
+++ My Java Practice Codes/SymbolPatternPrinter.java
@@ -10,13 +10,12 @@ public class SymbolPatternPrinter  {
 		// 4개 입력하면 "+-+-" 출력, 3개 입력하면 "+-+" 출력.
 		do {
 			System.out.print("값 입력:");
-			n = sc.nextInt();
-		} while(n < 0);
+			n = Integer.parseInt(sc.nextLine());
+		} while(n < 0\1);
 		
 		//  4를 입력했으면 "+-+-" 가 출력되어야 하는데, for문으로 1회 출력할 때마다 "+-"를 출력하기에 입력한 4의 절반만 for문을 실행. 
-		for(int i = 0; i < n / 2; i++) {
+		for(int i = 0; i < n / 2; i++)
 			System.out.print("+-");
-		}
 		
 		// 위에 for문은 오로지 "+-"만 출력한다. 홀수를 입력했으면 마지막에 "+"로 끝나기에 입력한 값이 홀수일 경우 "+"를 1개 출력.
 		if(n % 2 != 0)
